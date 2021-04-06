8da1d327e42aca1ed18afb26e93dbd09e3c6465a
Anshul Sanghi <xzaviourr@gmail.com>
2021-04-06 15:09:51 +0530

    Added connection file for database

---
+++ InternTracker/Database/connection.py
@@ -0,0 +1,18 @@
+import psycopg2
+import json
+
+class Database:
+    def __init__(self):
+        file = open('connection.config')
+        credentials_raw = file.read()
+        credentials_dict = json.loads(credentials_raw)
+
+        self.__dbname = str(credentials_dict["dbname"])
+        self.__user = str(credentials_dict["user"])
+        self.__password = str(credentials_dict["password"])
+
+    def connect(self):
+        connection = psycopg2.connect(f"dbname = {self.__dbname} user = {self.__user} password = {self.__password}")
+        return connection
+
+obj = Database()
