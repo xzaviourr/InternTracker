58c29cf3982c5ae5733c1b88d249a7be4bbd30a2
kailash360 <kailashkejriwal21@gmail.com>
2021-07-17 02:31:00 +0530

    Added proxy file path

--- InternTracker/InternTracker/settings.py
+++ InternTracker/InternTracker/settings.py
@@ -101,3 +101,7 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+# For proxies 
+ROTATING_PROXY_LIST_PATH = 'proxies.txt'
